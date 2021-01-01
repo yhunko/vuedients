f3e7fbe5be1f03afaf74c889b73cb0043d7fc48e
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-01-01 14:25:47 +0200

    Cleared default component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default Vue.extend({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 })
 </script>
 
